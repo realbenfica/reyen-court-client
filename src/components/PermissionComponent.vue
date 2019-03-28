e5f207cd8eaff341f6dee0e6a03a551082482094
realbenfica <alexgtr84@gmail.com>
2019-03-28 13:08:55 +0100

    Edit css

--- src/components/PermissionComponent.vue
+++ src/components/PermissionComponent.vue
@@ -74,22 +74,12 @@ div.permission {
   position: relative;
   border: 1px solid #5bd658;
   background-color: #bcffb8;
-  padding: 10px 10px 30px 10px;
+  padding: 5px 5px 10px 5px;
   margin-bottom: 15px;
 }
 
-div.created-at {
-  position: absolute;
-  top: 0;
-  left: 0;
-  padding: 5px 15px 5px 15px;
-  background-color: darkgreen;
-  color: white;
-  font-size: 13px;
-}
-
 p.text {
-  font-size: 22px;
+  font-size: 12px;
   font-weight: 700;
   margin-bottom: 0;
 }
